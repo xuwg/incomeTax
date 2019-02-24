0da7c5a124e8bd7412f311f59999332e9cbadbe3
xuwg <10461459@qq.com>
2019-02-24 11:41:44 +0800

    添加保险公积金详情

--- js/index.js
+++ js/index.js
@@ -49,6 +49,82 @@ window.onload = function () {
     select("fuJia");
     select("fuJiaAcc");
 
+    var baoXianDisplay = 0;
+    $("baoXianIco").onclick = function () {
+        if (baoXianDisplay) {
+            $("baoXianDetail").style.display = "none";
+            this.style.backgroundPosition = "0 0"; 
+            baoXianDisplay = 0;
+        } else {
+            $("baoXianDetail").style.display = "block";
+            this.style.backgroundPosition = "-32px 0";
+            baoXianDisplay = 1;
+        }
+    }
+
+    var gongJiJinDisplay = 0;
+    $("gongJiJinIco").onclick = function () {
+        if (gongJiJinDisplay) {
+            $("gongJiJinDetail").style.display = "none";
+            this.style.backgroundPosition = "0 0"; 
+            gongJiJinDisplay = 0;
+        } else {
+            $("gongJiJinDetail").style.display = "block";
+            this.style.backgroundPosition = "-32px 0";
+            gongJiJinDisplay = 1;
+        }
+    }
+
+    // 保险费总和
+    function setBaoXianFei() {
+        var yangLaoFei = $("yangLaoFei").value;
+        var shiYeFei = $("shiYeFei").value;
+        var gongShangFei = $("gongShangFei").value;
+        var shengYvFei = $("shengYvFei").value;
+        var yiLiaoFei = $("yiLiaoFei").value;
+
+        var baoXianFei = parseFloat(yangLaoFei==""?0:yangLaoFei) + parseFloat(shiYeFei==""?0:shiYeFei) 
+        + parseFloat(gongShangFei==""?0:gongShangFei) + parseFloat(shengYvFei==""?0:shengYvFei) + parseFloat(yiLiaoFei==""?0:yiLiaoFei);
+        $("baoXian").value = baoXianFei.toFixed(2);
+    }
+
+    //保险基数联动 保险费 
+    function baoXianJiShuLianDong(jishu, fei, rate, extra) {
+        $(jishu).oninput = function(){
+            var jiShu = $(jishu).value;
+            jiShu = jiShu==""?0:jiShu;
+            $(fei).value = (jiShu*rate+parseFloat(extra)).toFixed(2);
+    
+            setBaoXianFei();
+        }
+    }
+
+    // 养老
+    $("yangLaoFei").oninput = function(){
+        setBaoXianFei();
+    }    
+    baoXianJiShuLianDong("yangLaoJiShu", "yangLaoFei", 0.08, 0);
+    // 失业
+    $("shiYeFei").oninput = function(){
+        setBaoXianFei();
+    }
+    baoXianJiShuLianDong("shiYeJiShu", "shiYeFei", 0.002, 0);
+    // 工伤
+    $("gongShangFei").oninput = function(){
+        setBaoXianFei();
+    }
+    baoXianJiShuLianDong("gongShangJiShu", "gongShangFei", 0.0, 0);
+    // 生育
+    $("shengYvFei").oninput = function(){
+        setBaoXianFei();
+    }
+    baoXianJiShuLianDong("shengYvJiShu", "shengYvFei", 0.0, 0);
+    // 医疗
+    $("yiLiaoFei").oninput = function(){
+        setBaoXianFei();
+    }
+    baoXianJiShuLianDong("yiLiaoJiShu", "yiLiaoFei", 0.02, 3);
+
     // 根据输入计算累积，没有累计值的情况下，使用单个月的值乘以期数
     function prepare(input, acc, qiShu) {
         var inputValue = $(input).value;
@@ -70,7 +146,7 @@ window.onload = function () {
         // 总收入去掉免税额得到应税金额
         var taxShuoDe = parseFloat(gongZiAcc) + parseFloat(buZhuAcc) - 5000 * qiShu - baoXianAcc - gongJiJinAcc - fuJiaAcc;
 
-        return taxShuoDe>0 ? taxShuoDe:0;
+        return taxShuoDe > 0 ? taxShuoDe : 0;
     }
 
     // 获取前一个月的应税所得
@@ -88,7 +164,7 @@ window.onload = function () {
             // 总收入去掉免税额得到应税金额
             var taxShuoDe = parseFloat(gongZiAcc) + parseFloat(buZhuAcc) - 5000 * qiShu - baoXianAcc - gongJiJinAcc - fuJiaAcc;
 
-            return taxShuoDe>0 ? taxShuoDe:0;
+            return taxShuoDe > 0 ? taxShuoDe : 0;
         }
     }
 
@@ -101,14 +177,14 @@ window.onload = function () {
         $("taxLv").innerText = "适用税率" + taxLv + "%";
         var kouChuShu = GetKouChuShu(taxShuoDe);
         $("taxKouChu").innerText = "速算扣除数" + kouChuShu + "元";
-        var tax = (taxShuoDe * taxLv / 100-kouChuShu).toFixed(2);
+        var tax = (taxShuoDe * taxLv / 100 - kouChuShu).toFixed(2);
         $("yingJiaotax").value = tax; //累积应交税款
 
         // 前一个月应税所得
         var preMonthTaxShuoDe = GetPreMonthTaxShuoDe(qiShu);
-        var yiJiaoTax = (preMonthTaxShuoDe * GetTaxLv(preMonthTaxShuoDe) / 100-GetKouChuShu(preMonthTaxShuoDe)).toFixed(2); //已交税
-        $("yiJiaoTax").value = yiJiaoTax;//已缴税款
-        var tuiBuTax = (tax - yiJiaoTax).toFixed(2);//退补税款
+        var yiJiaoTax = (preMonthTaxShuoDe * GetTaxLv(preMonthTaxShuoDe) / 100 - GetKouChuShu(preMonthTaxShuoDe)).toFixed(2); //已交税
+        $("yiJiaoTax").value = yiJiaoTax; //已缴税款
+        var tuiBuTax = (tax - yiJiaoTax).toFixed(2); //退补税款
         $("tuiBuTax").value = tuiBuTax;
         // 收入
         $("shouRu").value = parseFloat($("gongZi").value - $("baoXian").value - $("gongJiJin").value - tuiBuTax + parseFloat($("buZhu").value)).toFixed(2);
